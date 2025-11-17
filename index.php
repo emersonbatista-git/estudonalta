ed5722207460807ff6874ec229d602041540d27d
Emerson Santos <ebsantoss@gmail.com>
2025-11-17 17:27:25 -0300

    ok

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo/estilo.css">
+    <title>Listagem de Jogos</title>
+</head>
+<body>
+    <div id="corpo">
+        <h1>Escolha seu Jogo</h1>
+        <table class="listagem">
+            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+        </table>
+
+    </div>
+    
+</body>
+</html>
