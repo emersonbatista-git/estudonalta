a2b7d480268d4005c7aa02364f0cd088eac56d9f
Emerson Santos <ebsantoss@gmail.com>
2025-11-19 07:56:02 -0300

    ok

--- includes/banco.php
+++ includes/banco.php
@@ -1,4 +1,3 @@
-<pre>
 <?php 
 //CONEXAO COM O BANCO DE DADOS
 $banco = new mysqli("localhost", "root","","bd_games");
@@ -11,15 +10,4 @@ $banco-> query("SET NAMES 'UTF8'");
 $banco-> query("SET character_set_connection=UTF8");
 $banco-> query("SET character_set_client=UTF8");
 $banco-> query("SET character_set_results=UTF8");
-
-//CRIAR UMA BUSCA E UTILIZAR A FUNÇÃO
-$busca = $banco->query("select * from generos");
-if (!$busca) {
-    echo "<p>Falha na busca! $banco->error</p>";
-}else {
-    while ($reg = $busca-> fetch_object()){;
-    print_r($reg);}
-}
-
-
 ?>
--- index.php
+++ index.php
@@ -7,13 +7,32 @@
     <title>Listagem de Jogos</title>
 </head>
 <body>
+    <?php
+    require_once "includes/banco.php"; 
+    ?>
     <div id="corpo">
         <h1>Escolha seu Jogo</h1>
         <table class="listagem">
-            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+            <?php
+            $busca = $banco->query("select * from jogos order by nome");
+              if (!$busca) {
+                echo "<tr><td>Infelizmente a busca deu errado";
+              } else {
+                  if ($busca->num_rows == 0) {
+                    echo "<tr><td>Nenhum registro encontrado";
+                  } else {
+                    while ($reg=$busca->fetch_object()) {
+                        echo "<tr><td>$reg->capa<td>$reg->nome";
+                        echo "<td>Adm";    
+                    }
+                  }
+                } 
+              
+            ?>
         </table>
 
     </div>
+    <?php $banco->close();?>
     
 </body>
 </html>
