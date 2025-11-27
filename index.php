23a9920ba4c68c03ebf723e9faf8373d8d3a6da3
Emerson Santos <ebsantoss@gmail.com>
2025-11-27 10:25:51 -0300

    ok

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de Jogos</title>
+    <link rel="stylesheet" href="estilos/estilo.css">
+</head>
+<body>
+    <div id="corpo">
+        <h1>Escolha seu Jogo</h1>
+      
+        <table classe="listagem">
+            <tr><td>Foto<td>Nome<td>Adm
+            <tr><td>Foto<td>Nome<td>Adm
+            <tr><td>Foto<td>Nome<td>Adm
+            <tr><td>Foto<td>Nome<td>Adm
+
+        </table>
+
+    </div>
+</body>
+</html>
---
+++ modelo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo da Pagina</title>
+    <link rel="stylesheet" href="estilos/estilo.css">
+</head>
+<body>
+    <div id="corpo">
+        <h1>Olá!</h1>
+
+    </div>
+</body>
+</html>
