d3e4b1560234c4c0aa54bc3dbc7cd38d6e04ff4c
Emerson Santos <ebsantoss@gmail.com>
2026-07-15 05:19:26 -0300

    improviso

---
+++ udemy/ex/js/script.js
@@ -0,0 +1,15 @@
+const numero = Number(prompt('Digite um numero: '))
+
+const numeroTitulo =window.document.getElementById('numero-titulo');
+
+const texto =window.document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = '';
+texto.innerHTML += `<p>Raiz quadrada ${numero ** 0.5}</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>É NaN ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
+
